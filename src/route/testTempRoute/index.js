0e7c11acfe0f043f7d0a5346975773c489d7cb57
Andri Purnomo <andri.adrp@gmail.com>
2023-02-16 16:49:45 +0700

    feat: request and response validation with JSON schema

--- src/route/testTempRoute/index.js
+++ src/route/testTempRoute/index.js
@@ -1,22 +1,37 @@
 const tempService = require('../../services/temp.service');
+const {
+  postRequestbody,
+  postReponse,
+  getResponseBody,
+} = require('./temp.schema');
 
 const route = async (fastify) => {
   const { getAll, save } = tempService(fastify);
 
-  fastify.get('/', async (request, reply) => {
-    const allTest = await getAll();
+  fastify.get(
+    '/',
+    { schema: { response: getResponseBody } },
+    async (request, reply) => {
+      const allTest = await getAll();
 
-    reply.code(200).send(allTest);
-  });
+      reply.code(200).send({
+        temps: allTest,
+      });
+    }
+  );
 
-  fastify.post('/', async (request, reply) => {
-    fastify.log.info(`request with body ${request}`);
-    const { title } = request.body;
+  fastify.post(
+    '/',
+    { schema: { body: postRequestbody, response: postReponse } },
+    async (request, reply) => {
+      fastify.log.info(`request with body ${request}`);
+      const { title } = request.body;
 
-    const id = await save(title);
+      const id = await save(title);
 
-    reply.code(201).send({ id });
-  });
+      reply.code(201).send(id);
+    }
+  );
 };
 
 module.exports = route;
